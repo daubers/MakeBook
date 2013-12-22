8d51be5330ec79181d0ab1f223788c495185b839
Matt Daubney <matt@gblabs.co.uk>
2013-12-22 22:23:06 +0000

    Index page mostly working Added Datatables JS/CSS files and related Bootstrap additions

--- Project/views.py
+++ Project/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render_to_response
+from models import Project, Part, BoMToParts
 
 # Create your views here.
+
 
 def index(request):
     """
         The main index page
     """
     datadict = {}
-
+    projects = Project.objects.all()[:5]
+    orders = BoMToParts.objects.filter(ordered=True).all()[:5]
+    deliveries = BoMToParts.objects.filter(ordered=True, arrived=False).all()[:5]
+    datadict['projects'] = projects
+    datadict['orders'] = orders
+    datadict['deliveries'] = deliveries
     return render_to_response('Project/index.html', datadict)
